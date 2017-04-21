08ef7c9e3e107a469575776865a3387e4b9e5952
Guillaume Girard <guillaumelgirard@gmail.com>
2017-04-21 16:17:00 -0400

    Added parsing for survey and reviews
    added code to parse survey csv and reviews from Play Store

--- reviews_parsing/main.py
+++ reviews_parsing/main.py
@@ -1,19 +1,38 @@
+from glob import glob
 import pandas as pd
 from nltk.corpus import stopwords
 import nltk.tokenize
+from collections import Counter
 
-r = open("./files/TMNT/reviews%2Freviews_com.ludia.tmnt_201704.csv", "rt", encoding="utf16")
-df = pd.read_csv(r)
+path = "./files/TMNT"
+allfiles = glob(path + "/*.csv")
+df = pd.DataFrame()
+list_ = []
+for file_ in allfiles:
+    r = open(file_, "rt", encoding="utf16")
+    df = pd.read_csv(r)
+    list_.append(df)
+df = pd.concat(list_)
 
 cols = list(df.loc[:, 'Package Name':'Device']) + list(df.loc[:, 'Star Rating':'Review Text'])
 df_working = df[cols]
 df_working = df_working.dropna(subset=['Review Text'])
-corpus = df_working['Review Text']
-
+df_corpus = df_working['Review Text']
 
 tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 
-tokenizedDF = tokenizer.tokenize(str(corpus))
-print(tokenizedDF)
+df_tokenized = tokenizer.tokenize(str(df_corpus))
 
-stops = set(nltk.stopwords.words('english'))
+languages = {'english', 'spanish', 'russian', 'portuguese'}
+
+stops = []
+
+for lang in languages:
+    stops.append(set(stopwords.words(lang)))
+
+filtered_words = [w for w in df_tokenized if w not in stops]
+
+freqs = Counter(filtered_words)
+
+df_word = pd.DataFrame.from_dict(freqs, orient="index").reset_index().sort(0, ascending=False)
+print(df_word)
--- survey_parsing/main.py
+++ survey_parsing/main.py
@@ -1,5 +1,61 @@
 import pandas as pd
-import stop_words as sw
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("./files/persona_definition_survey.csv")
-print(df.head())
+df = pd.read_csv("./files/persona_definition_survey.csv")\
+    .rename(columns=lambda x: x.strip())
+
+print(list(df))
+
+colstart = input("Copy first column without quotation: ")
+colend = input("Copy last column without quotation: ")
+
+cols = list(df.loc[:, colstart:colend])
+
+df_filtered = df[cols].dropna()
+
+colrank = ['Rank the following game aspects in order of importance.  [Graphics]',
+           'Rank the following game aspects in order of importance.  [GamePlay]',
+           'Rank the following game aspects in order of importance.  [User Interface]',
+           'Rank the following game aspects in order of importance.  [Collection]',
+           'Rank the following game aspects in order of importance.  [Competition]',
+           'Rank each type of event in order of preference [Limited Time Battle Events '
+           + '(Rarity Rumble/Battle for Survival/Mighty Money/]',
+           'Rank each type of event in order of preference [Preservation/Stampede Events]',
+           'Rank each type of event in order of preference [PvP Arenas]',
+           'Rank each type of event in order of preference [Tournaments]',
+           'Rank each type of event in order of preference [Community Events]']
+
+for col in df_filtered.columns:
+    if col in colrank:
+        df_filtered[col] = [s[-1:] for s in df_filtered[col]]
+
+for col in df_filtered.columns:
+    if col in colrank:
+        df_filtered[col] = df_filtered[col].apply(pd.to_numeric)
+
+print(list(df_filtered))
+
+grouping = input("Enter the column to aggregate with: ")
+plot_average = ""
+plot_median = []
+
+for col in df_filtered.columns:
+    if col in colrank:
+        average = df_filtered.groupby(grouping).agg({col: np.average})
+        median = df_filtered.groupby(grouping).agg({col: np.median})
+        plot_average.append(average)
+        #plot_median.append(median)
+
+pay_average = df_filtered.groupby('What is your age?')\
+    .agg({'Rank the following game aspects in order of importance.  [Graphics]': np.average,
+          'Rank the following game aspects in order of importance.  [GamePlay]': np.average,
+          'Rank the following game aspects in order of importance.  [User Interface]': np.average,
+          'Rank the following game aspects in order of importance.  [Collection]': np.average,
+          'Rank the following game aspects in order of importance.  [Competition]': np.average})
+print(pay_average)
+
+#plot_average.plot()
+print(plot_average)
+#plot_median.plot()
+#plt.show()
